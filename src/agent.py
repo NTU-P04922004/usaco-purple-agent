447a15f5ff47e10f98b9cacd5622bc19ae4b09ab
Kuo-Hsin Tu <dapumptu@gmail.com>
2026-01-15 11:00:53 +0000

    Use LangChain for agent

--- src/agent.py
+++ src/agent.py
@@ -1,9 +1,19 @@
+import os
+import re
+
 from a2a.server.tasks import TaskUpdater
 from a2a.types import Message, Part, TaskState, TextPart
 from a2a.utils import get_message_text, new_agent_text_message
+from langchain_sambanova import ChatSambaNova
 from loguru import logger
 
-from graph import graph
+from prompt import DIRECT_PROMPT
+
+
+def extract_python_code_block(markdown_string):
+    pattern = r"```python\s*\n(.*?)```"
+    code_blocks = re.findall(pattern, markdown_string, flags=re.DOTALL | re.IGNORECASE)
+    return code_blocks[0] if code_blocks else None
 
 
 class Agent:
@@ -25,12 +35,16 @@ class Agent:
             TaskState.working, new_agent_text_message("Thinking...")
         )
 
-        initial_state = {"problem_description": input_text, "reflections": []}
-        result = graph.invoke(initial_state)
-        response = result["code"]
-        logger.info(f"Agent: {response}")
+        model = ChatSambaNova(
+            model="Meta-Llama-3.3-70B-Instruct", max_tokens=8192, temperature=0
+        )
+        prompt = DIRECT_PROMPT.format(problem=input_text)
+        result = model.invoke(prompt)
+        code = extract_python_code_block(result.content)
+
+        logger.info(f"Agent: {code}")
 
         await updater.add_artifact(
-            parts=[Part(root=TextPart(text=response))],
-            name="Response",
+            parts=[Part(root=TextPart(text=code))],
+            name="Solution",
         )
